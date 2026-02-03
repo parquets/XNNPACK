3811da0748efa289cd1c9bc4bdd49a5210447010
XNNPACK Team <xnnpack-github-robot@google.com>
2026-02-02 23:16:08 -0800

    Add pf32 support for f32_f16 FC node to enable SME acceleration.
    f16 weights are converted to f32 internally before actual matmul, so it's straightforward to make it support SME as what have done for pure f32 case.
    
    PiperOrigin-RevId: 864709228

--- src/subgraph.c
+++ src/subgraph.c
@@ -3860,7 +3860,8 @@ enum xnn_status xnn_subgraph_optimize_packed_lhs(xnn_subgraph_t subgraph,
             break;
           case xnn_datatype_fp32:
             if (input_datatype == output_datatype &&
-                kernel_datatype == xnn_datatype_fp32) {
+                (kernel_datatype == xnn_datatype_fp32 ||
+                  kernel_datatype == xnn_datatype_fp16)) {
               if ((gemm_config = xnn_init_pf32_gemm_config())) {
                 assumed_datatype = xnn_datatype_pfp32;
               }
@@ -4021,7 +4022,8 @@ enum xnn_status xnn_subgraph_optimize_packed_lhs(xnn_subgraph_t subgraph,
         }
 
         if (input_datatype == xnn_datatype_fp32 &&
-            kernel_datatype == xnn_datatype_fp32 &&
+            (kernel_datatype == xnn_datatype_fp16 ||
+             kernel_datatype == xnn_datatype_fp32) &&
             output_datatype == xnn_datatype_fp32 &&
             xnn_init_pf32_gemm_config() != NULL &&
             !(optimization_flags & XNN_FLAG_NO_INLINED_LHS_PACKING)) {
